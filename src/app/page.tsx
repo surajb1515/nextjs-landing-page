da64e4659fcb0e05a267fb95ab79814d8d3acf0c
Suraj Bhardwaj <bhardwajsuraj1507@gmail.com>
2024-07-04 15:33:47 +0530

    added FeatureRight FeatureLeft FeatureSet.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 import { Main, Section, Container } from "@/components/craft";
+import FeatureLeft from "@/components/home-page/feature-left";
+import FeatureRight from "@/components/home-page/feature-right";
+import FeatureSet from "@/components/home-page/feature-set";
 import Hero from "@/components/home-page/hero";
 
 export default function Page() {
@@ -7,6 +10,9 @@ export default function Page() {
       <Section>
         <Container>
           <Hero />
+          <FeatureLeft />
+          <FeatureRight />
+          <FeatureSet />
         </Container>
       </Section>
     </Main>
